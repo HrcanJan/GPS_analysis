c7527d1258cc24e775cb323dabae2d85014d197d
Jan Hrcan <jankochrtan555@gmail.com>
2023-12-07 20:58:45 +0100

    fix: pyvis

--- DP Projekt/visualize/vizualize_using_pyvis.py
+++ DP Projekt/visualize/vizualize_using_pyvis.py
@@ -10,21 +10,24 @@ def load_csv(file_path):
 
 	return edges
 
-def visualize_graph(edges):
+def visualize_graph(edges, nodes):
 	G = nx.Graph()
+	G.add_nodes_from(map(str, nodes))
 	G.add_edges_from(edges)
 
 	nt = Network(directed=False, notebook=True)
 
 	for node in G.nodes:
-		nt.add_node(node, title=str(node), label=str(node))
+		nt.add_node(str(node), title=str(node), label=str(node))
 
 	nt.from_nx(G)
 
-	nt.show('./files/graphs/graph.html')
+	nt.show('./files/graphs/graph3.html')
 
 if __name__ == "__main__":
 	# ADJUST FILE PATH HERE
-	csv_file_path = './files/edges/geolife.csv'
-	edges = load_csv(csv_file_path)
-	visualize_graph(edges)
+	node_path = './files/nodes/geolife.csv'
+	edges_path = './files/edges/geolife_time_1min.csv'
+	edges = load_csv(edges_path)
+	nodes = load_csv(node_path)
+	visualize_graph(edges, nodes)
